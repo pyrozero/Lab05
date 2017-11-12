13160953cd3106dd858a0a0a39cae4653ba76908
Falish <pyro.zero@hotmail.com>
2017-11-12 17:54:44 +0700

    การบังคับให้เกิด Exception ในกรณีที่มีความผิดปกติในการแปลงชนิดข้อมูล

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,11 +3,16 @@ class Program
 {
     static void Main(string[]args)
     {
-        double d = 5e9d;
-        Console.WriteLine(d);
-        int i = (int)d;
-        Console.WriteLine(i);
-
-
+        try
+        {
+            double d = 5e9d;
+            Console.WriteLine(d);
+            int i = checked((int)d);
+            Console.WriteLine(i);
+        }
+        catch(OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }        
     }
 }
