50eea3443b92c38d165a4265554e1634b6951d8c
Falish <pyro.zero@hotmail.com>
2017-11-12 17:40:27 +0700

    ตัวดำเนินการตรรกะ (Logical operators)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,13 +3,17 @@ class Program
 {
     static void Main(string[]args)
     {
-        int x = 10, y = 5;
-        Console.WriteLine("x > y : " + (x > y));
-        Console.WriteLine("x < y : " + (x < y));
-        Console.WriteLine("x >= y : " + (x >= y));
-        Console.WriteLine("x <= y : " + (x <= y));
-        Console.WriteLine("x == y : " + (x == y));
-        Console.WriteLine("x != y : " + (x != y));
+        bool a = true;
+        bool b = false;
+        Console.WriteLine(a && b);
+        Console.WriteLine(a || b);
+        Console.WriteLine(!b);
+        Console.WriteLine(b || true);
+        Console.WriteLine((9 > 7) ^ (a == b));
+        Console.WriteLine((b != a) || (a == b));
+        Console.WriteLine((b != a) && (a == b));
+        Console.WriteLine((!a) && (!b));
+        Console.WriteLine((!a) || (!b));
 
     }
 }
