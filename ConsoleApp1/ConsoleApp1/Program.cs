f4be0cb03b20430c952b9b17689441e28e9c24b2
Falish <pyro.zero@hotmail.com>
2017-11-12 17:50:59 +0700

    การบังคับให้เกิด Exception ในกรณีที่มีความผิดปกติในการแปลงชนิดข้อมูล

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,20 +3,11 @@ class Program
 {
     static void Main(string[]args)
     {
-        long myLong = long.MaxValue;
-        int myInt = (int)myLong;   // Explicit conversion
+        double d = 5e9d;
+        Console.WriteLine(d);
+        int i = (int)d;
+        Console.WriteLine(i);
 
-        float heightInMeters = 1.74f; // Explicit conversion
-        double maxHeight = heightInMeters; // Implicit
-        double minHeight = (double)heightInMeters; // Explicit
-        float actualHeight = (float)maxHeight; // Explicit
-
-        Console.WriteLine(myLong);
-        Console.WriteLine(myInt);
-        Console.WriteLine(heightInMeters);
-        Console.WriteLine(maxHeight);
-        Console.WriteLine(minHeight);
-        Console.WriteLine(actualHeight);
 
     }
 }
