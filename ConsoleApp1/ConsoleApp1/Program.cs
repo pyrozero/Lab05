e01ffe0be4d72a58bcacfc583818649901d54678
Falish <pyro.zero@hotmail.com>
2017-11-12 15:20:27 +0700

    lab05

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        int c;
+        c = add(a, b);
+        Console.WriteLine("c = {0}", c);
+        Console.WriteLine("3 + 5 = {0}", add(3, 5));
+        Console.WriteLine("4 + 7 =  " + add(4, 7));
+        Console.ReadLine();
+    }
+
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
