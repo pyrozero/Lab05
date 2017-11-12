e89981b1db645094bca4db1770dfa2e089c6d5ad
Falish <pyro.zero@hotmail.com>
2017-11-12 18:06:06 +0700

    test01

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,11 +3,34 @@ class Program
 {
     static void Main(string[]args)
     {
-        int a = 2;
-        float f = 3.5f;
-        double d = 4.99d;
-        decimal money = 1254841354;
-        Console.WriteLine(a + " " + f + " " + d + " " + money);
+      try
+        {
+            Console.Write("Enter value : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            EventCheck(val1);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    static int EventCheck(int val1)
+    {
+        if (val1 % 2 == 0)
+        {
+            Console.WriteLine("{0} is Event Number", val1);
+        }
+        else
+        {
+            Console.WriteLine("{0} is Odd Number", val1);
+        }
+        return (0);
 
     }
+
 }
